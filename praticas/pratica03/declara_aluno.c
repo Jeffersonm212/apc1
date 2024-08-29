020c3fe9bc1e5fe9e3b768de55aa3c9793de5450
Jeffersonm212 <jeffersonmarques429@gmail.com>
2024-08-29 20:14:57 +0000

    Adicionando arquivos da pratica03

--- praticas/pratica03/declara_aluno.c
+++ praticas/pratica03/declara_aluno.c
@@ -2,14 +2,17 @@
 
 int main() {
 
-  unsigned long int  Matricula = 00000000;
-    printf("Matrícula: %08lu\n", Matricula);
+  unsigned long int  matricula = 00000000;
+    printf("Matrícula: %08lu\n", matricula);
   unsigned  int idade = 99;
-    printf("Idade: %i anos\n", idade);
-  unsigned int Altura = 0.00;
-    printf("Altura: %1.2f m\n", Altura);
-  float Peso = 0000.0;
-  printf( "Peso: %04.1f kg\n", Peso);
-  
+    printf("Idade: %u anos\n", idade);
+  unsigned int altura = 9.90;
+    printf("Altura: %3.1f m\n", altura);
+  float peso = 999.0; 
+  printf("Peso: %.1f kg\n", peso);
+  char sexo = 'F';  
+
+  printf("Sexo: %c\n", sexo);
+
   return 0;
   }
